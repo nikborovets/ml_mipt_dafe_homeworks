ff569437049d507cd5cf5b94d52e3828487ffc3f
Nick Borovets <nborovets@yandex.ru>
2025-05-28 09:40:56 +0300

    add sep func in prepare_data

--- homeworks_dl/01_Pytorch_NN/homework_example_project/prepare_data.py
+++ homeworks_dl/01_Pytorch_NN/homework_example_project/prepare_data.py
@@ -1,5 +1,10 @@
 from torchvision.datasets import CIFAR10
 
-if __name__ == "__main__":
+def prepare_data():
     train_dataset = CIFAR10("CIFAR10/train", download=True)
     test_dataset = CIFAR10("CIFAR10/test", download=True)
+
+    return train_dataset, test_dataset
+
+if __name__ == "__main__":
+    prepare_data()
